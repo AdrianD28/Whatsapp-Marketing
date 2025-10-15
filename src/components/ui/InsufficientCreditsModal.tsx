ecd2a7350a768fad0e18498caf2c47a02679035a
Adrian Rincon <adrianrincon102001@gmail.com>
2025-10-15 08:02:32 -0400

    Fix: Corrección de imports en InsufficientCreditsModal

--- src/components/ui/InsufficientCreditsModal.tsx
+++ src/components/ui/InsufficientCreditsModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AlertTriangle } from 'lucide-react';
-import Modal from './Modal';
-import Button from './Button';
+import { Modal } from './Modal';
+import { Button } from './Button';
 
 interface InsufficientCreditsModalProps {
   isOpen: boolean;
